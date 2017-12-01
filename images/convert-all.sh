19a6d6df4bb8fcfb3b97fa069260e3cfeffcd691
rudametw <rudametkin@gmail.com>
2017-12-01 16:19:21 +0100

    initial import of git course

---
+++ images/convert-all.sh
@@ -0,0 +1,5 @@
+inkscape -A file_share.pdf file_share.svg
+inkscape -A file_share_many.pdf file_share_many.svg
+inkscape -A file_versions.pdf file_versions.svg
+inkscape -A services.pdf services.svg
+inkscape -A gestion_versions.pdf gestion_versions.svg
